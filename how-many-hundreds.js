2305de1029e021679fc50d605202fdcb0af443b5
hyuntony <ankim90@gmail.com>
2021-04-29 03:57:18 -0400

    how many hundreds assignment

---
+++ how-many-hundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(num) {
+  const remainder = num % 100;
+  num -= remainder;
+  return num / 100;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
