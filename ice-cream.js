acc298d1f9a1db1a92a1d0857a1249717fa572ae
hyuntony <ankim90@gmail.com>
2021-04-30 04:02:03 -0400

    modified loop to be a while loop packing list

--- ice-cream.js
+++ ice-cream.js
@@ -2,3 +2,6 @@ const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky ro
 console.log(iceCreamFlavours);
 iceCreamFlavours.push("root beer");
 console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
